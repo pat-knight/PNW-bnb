cae340d97c8cc6015acb8e3ad19c77e5aaf0a946
Patrick Knight <ptknight00@gmail.com>
2018-05-14 20:41:24 -0500

    appbase components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,99 @@
 import React, { Component } from 'react';
 import './App.css';
-import { ReactiveBase } from '@appbaseio/reactivesearch';
+import { ReactiveBase, NumberBox, DateRange, RangeSlider, ResultCard } from '@appbaseio/reactivesearch';
 
 class App extends Component {
-  render() {
-    return (
-      <section className='container'>
+render() {
+return (
+    <div className="container">
         <ReactiveBase
-          app='listings'
-          credentials="2i2gQJ4bT:18294d70-14c0-4945-afb0-14a5046ad746"
-          type='listing'
-          >
-          Linking ReactiveSearch
+            app="housing"
+            credentials="0aL1X5Vts:1ee67be1-9195-4f4b-bd4f-a91cd1b5e4b5"
+            type="listing"
+            theme={{
+                primaryColor: '#08b34f',
+            }}
+        >
+            <nav className="nav">
+                <div className="title">PNW-bnb</div>
+            </nav>
+            <div className="left-col">
+                <DateRange
+                    dataField="date_from"
+                    componentId="DateRangeSensor"
+                    title="When"
+                    numberOfMonths={2}
+                    queryFormat="basic_date"
+                    initialMonth={new Date('05-14-2018')}
+                />
+
+                <NumberBox
+                    componentId="GuestSensor"
+                    dataField="accommodates"
+                    title="Guests"
+                    defaultSelected={2}
+                    labelPosition="right"
+                    data={{
+                        start: 1,
+                        end: 16,
+                    }}
+                />
+
+                <RangeSlider
+                    componentId="PriceSensor"
+                    dataField="price"
+                    title="Price Range"
+                    range={{
+                        start: 10,
+                        end: 250,
+                    }}
+                    rangeLabels={{
+                        start: '$10',
+                        end: '$250',
+                    }}
+                    defaultSelected={{
+                        start: 10,
+                        end: 50,
+                    }}
+                    stepValue={10}
+                    interval={20}
+                    react={{
+                        and: ['DateRangeSensor'],
+                    }}
+                />
+            </div>
+
+            <ResultCard
+                className="right-col"
+                componentId="SearchResult"
+                dataField="name"
+                size={12}
+                onData={data => ({
+                    image: data.image,
+                    title: data.name,
+                    description: (
+                        <div>
+                            <div className="price">${data.price}</div>
+                            <p className="info">{data.room_type} · {data.accommodates} guests</p>
+                        </div>
+                    ),
+                    url: data.listing_url,
+                })}
+                pagination
+                react={{
+                    and: ['GuestSensor', 'PriceSensor', 'DateRangeSensor', 'search'],
+                }}
+                innerClass={{
+                    resultStats: 'result-stats',
+                    list: 'list',
+                    listItem: 'list-item',
+                    image: 'image',
+                }}
+            />
         </ReactiveBase>
-      </section>
-    );
-  }
+    </div>
+);
+}
 }
 
 export default App;
---
+++ src/Components/UI/DateRange/DateRange.js
@@ -0,0 +1,19 @@
+// import { React, Component } from 'react';
+// import { DateRange } from '@appbaseio/reactivesearch';
+
+// class Daterange extends Component {
+//     render() {
+//         return (
+//             <DateRange
+//                 dataField='date_from'
+//                 componentId='DateRangeSensor'
+//                 title='When'
+//                 numberOfMonths={1}
+//                 queryFormat='basic_date'
+//             />
+//         )
+//     }
+// }
+
+
+// export default Daterange;
